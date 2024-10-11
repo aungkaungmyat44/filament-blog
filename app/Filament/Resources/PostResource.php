e7fdfc106bcbc53f436fbacc9ed834c6024d2681
Aung Kaung Myat <aungkaungmyat44252@gmail.com>
2024-10-11 15:35:54 +0630

    Added- filter on post table

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -26,6 +26,9 @@ use Filament\Tables\Columns\CheckboxColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -122,7 +125,23 @@ class PostResource extends Resource
                           ->toggleable(),                        
             ])
             ->filters([
-                //
+                /*
+                Filter::make('Published Posts')
+                ->query(function ($query) {
+                    $query->where('published', true);
+                }),
+                Filter::make('Unpublished Posts')
+                ->query(function ($query) {
+                    $query->where('published', false);
+                }),
+                */
+                TernaryFilter::make('published'),
+                SelectFilter::make('category_id')
+                ->label('Category')
+                ->multiple()
+                ->searchable()
+                ->preload()
+                ->relationship('category', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
